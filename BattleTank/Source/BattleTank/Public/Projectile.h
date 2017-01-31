0b243dac6540ac65d5cc926fc526b5f3ad79d78c
OWNER-PC\Owner <tim.k96@hotmail.com>
2017-02-01 02:50:16 +1100

    Working on Animation

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -17,9 +17,6 @@ public:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	
-	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
-
 	void LaunchProjectile(float Speed);
 
 private:
@@ -30,6 +27,12 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Setup")
 		UParticleSystemComponent* LaunchBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Setup")
+		UParticleSystemComponent* ImpactBlast = nullptr;
+
+	UFUNCTION()
+		virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 	
 	
 };
